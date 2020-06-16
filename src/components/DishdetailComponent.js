82b09ccf75f8ddfa6bd57c500060b6363967ddff
Aditya Narayan Sah <adityasah73.as@gmail.com>
2020-06-17 00:47:27 +0530

    Single Page Application

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,55 +1,77 @@
 import React from 'react';
-import {Card, CardImg, CardText, CardBody, CardTitle} from 'reactstrap';
+import {Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem} from 'reactstrap';
+import {Link} from 'react-router-dom';
 
 function RenderDish({dish}) {
 
-    console.log('Dishdetail Component Didrender');
+    return(
+            <div>
+                <Card>
+                    <CardImg src={dish.image} alt={dish.name}/>
+                    <CardBody>
+                        <CardTitle>{dish.name}</CardTitle>
+                        <CardText>{dish.description}</CardText>
+                    </CardBody>
+                </Card>
+            </div>
+    );
+}
 
-    if(dish != null) {
+function RenderComments({comments}) {
 
-        const Comments = dish.comments.map((comment) => {
-            return (
-                <li>{comment.comment}<br/>
-                <br/>-- {comment.author},
-                &nbsp;
-                {new Intl.DateTimeFormat('en-US', {
-                        year: 'numeric',
-                        month: 'long',
-                        day: '2-digit'
-                    }).format(new Date(comment.date))}<br/><br/>
-                </li>
-            );
-        });
-        return(
-            <div keys={dish.id} className="row">
-                <div className="col-12 col-md-5 m-1">
-                    <Card>
-                        <CardImg width="100%" object src={dish.image} alt={dish.name}/>
-                        <CardBody>
-                            <CardTitle>{dish.name}</CardTitle>
-                            <CardText>{dish.description}</CardText>
-                        </CardBody>
-                    </Card>
-                </div>
-                <div className="col-12 col-md-5 m-1">
-                    <h4>Comments</h4>
-                    <ul className="list-inline">{Comments}</ul>
-                </div>
+    if (comments != null)
+        return (
+            <div>
+                <h4>Comments</h4>
+                <ul className="list-unstyled">
+                    {comments.map((comment) => {
+                        return (
+                            <li key={Comment.id}>
+                                <p>{comment.comment}</p>
+                                <p>-- {comment.author},
+                                        &nbsp;
+                                        {new Intl.DateTimeFormat('en-US', {
+                                                year: 'numeric',
+                                                month: 'long',
+                                                day: '2-digit'
+                                            }).format(new Date(comment.date))}
+                                </p>
+                            </li>
+                        );
+                    })}
+                </ul>
             </div>
         );
-    }
-    else {
-        return(
+    else 
+        return (
             <div></div>
         );
-    }
+
 }
 
 const Dishdetail = (props) => {
     return (
-    <div className="container">
-        <RenderDish dish={props.dish}/>
-    </div>
+        <div className="container">
+        <div className="row">
+            <Breadcrumb>
+
+                <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+                <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+            </Breadcrumb>
+            <div className="col-12">
+                <h3>{props.dish.name}</h3>
+                <hr />
+            </div>                
+        </div>
+        <div className="row">
+            <div className="col-12 col-md-5 m-1">
+                <RenderDish dish={props.dish} />
+            </div>
+            <div className="col-12 col-md-5 m-1">
+                <RenderComments comments={props.comments} />
+            </div>
+        </div>
+        </div>
     );
 }
 
