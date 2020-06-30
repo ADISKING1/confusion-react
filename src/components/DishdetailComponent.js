c8d0d29a939eaea73fe6a0d2c8be6103d0bf3165
Aditya Narayan Sah <adityasah73.as@gmail.com>
2020-07-01 01:36:10 +0530

    Fetch from Server

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -3,6 +3,7 @@ import {Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem
 import {Link} from 'react-router-dom';
 import {Control, LocalForm, Errors} from 'react-redux-form';
 import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
 
 
 const required = (val) => val && val.length;
@@ -104,7 +105,7 @@ function RenderDish({dish}) {
     return(
             <div>
                 <Card>
-                    <CardImg src={dish.image} alt={dish.name}/>
+                    <CardImg src={baseUrl + dish.image} alt={dish.name}/>
                     <CardBody>
                         <CardTitle>{dish.name}</CardTitle>
                         <CardText>{dish.description}</CardText>
